970527f837a159007c3b1334d3579e1be1ff5881
Kirtan Vyas <kirtanv.itpath@gmail.com>
2025-07-15 16:21:19 +0530

    Setup basic axios and client configuration.

---
+++ src/api/client.ts
@@ -0,0 +1,100 @@
+import axios from "axios";
+
+export const METHODS = {
+  POST: "post",
+  GET: "get",
+  DELETE: "delete",
+  PUT: "put",
+  PATCH: "patch",
+  HEAD: "head",
+  OPTIONS: "options",
+};
+
+const axiosConfig = {
+  baseURL: "https://jsonplaceholder.typicode.com",
+  withCredentials: true,
+};
+const axiosInstance = axios.create(axiosConfig);
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const { response } = error;
+
+    if (!response) {
+      return Promise.reject(error);
+    }
+
+    const { status } = response;
+
+    const errorMessage = response?.error?.message;
+
+    const customErrorMessage = {
+      message: errorMessage,
+      ...response,
+    };
+
+    return Promise.reject(customErrorMessage);
+  }
+);
+
+const client = ({
+  method = METHODS.GET,
+  url = "",
+  data = undefined as any,
+  ...rest
+}: {
+  method?: string;
+  url?: string;
+  data?: any;
+  [key: string]: any;
+}) => {
+  return axiosInstance({
+    method,
+    url,
+    data,
+    withCredentials: true,
+    ...rest,
+  });
+};
+
+export default client;
+
+/*
+const client = ({
+  method = METHODS.GET,
+  url = "",
+  data = undefined as any,
+  ...rest
+}: {
+  method?: string;
+  url?: string;
+  data?: any;
+  [key: string]: any;
+})
+
+same as
+
+type ClientConfig =  {
+  method?: string;
+  url?: string;
+  data?: any;
+  [key: string]: any;
+}
+
+const client = ({
+  method = METHODS.GET,
+  url = '',
+  data = undefined as any,
+  ...rest
+}: ClientConfig) 
+*/
