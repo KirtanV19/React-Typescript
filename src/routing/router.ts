f60db3cb12c07c24284ce6c99f28ebdd38eaf109
Kirtan Vyas <kirtanv.itpath@gmail.com>
2025-07-16 15:54:30 +0530

    Implement auth_routes

--- src/routing/router.ts
+++ src/routing/router.ts
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
-import { PLAIN_ROUTES } from "./routes";
+import { PLAIN_ROUTES, AUTH_ROUTES } from "./routes";
 import Home from "../pages/home";
 import EmailVerification from "../pages/email-verification";
 import PrivacyPolicy from "../pages/privacy-policy";
 import TermsAndConditions from "../pages/terms-and-conditions";
+import Login from "../pages/auth/login";
+import ForgotPassword from "../pages/auth/forgot-password";
+import Register from "../pages/auth/register";
+import ResetPassword from "../pages/auth/reset-password";
+import PageNotFound from "../components/page-not-found";
 
 export const router = createBrowserRouter([
   {
@@ -15,4 +20,14 @@ export const router = createBrowserRouter([
       { ...PLAIN_ROUTES.TERMS_AND_CONDITIONS, Component: TermsAndConditions },
     ],
   },
+  {
+    ...AUTH_ROUTES.layout,
+    children: [
+      { ...AUTH_ROUTES.LOGIN, Component: Login },
+      { ...AUTH_ROUTES.FORGOT_PASSWORD, Component: ForgotPassword },
+      { ...AUTH_ROUTES.REGISTER, Component: Register },
+      { ...AUTH_ROUTES.RESET_PASSWORD, Component: ResetPassword },
+    ],
+  },
+  { path: "*", Component: PageNotFound },
 ]);
--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -30,3 +30,27 @@ export const PLAIN_ROUTES = {
     url: "/terms-and-conditions",
   },
 };
+
+export const AUTH_ROUTES = {
+  layout: {},
+  LOGIN: {
+    title: "Login",
+    path: "/login",
+    url: "/login",
+  },
+  REGISTER: {
+    title: "Register",
+    path: "/register",
+    url: "/register",
+  },
+  FORGOT_PASSWORD: {
+    title: "Forgot Password",
+    path: "/forgot-password",
+    url: "/forgot-password",
+  },
+  RESET_PASSWORD: {
+    title: "Reset Password",
+    path: "/reset-password",
+    url: "/reset-password",
+  },
+};
